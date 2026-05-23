d9086be858aa130200ad286f84970461563531f2
Сайт 1 <mirzatesenjanov1@gmail.com>
2026-05-23 23:15:38 +0600

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,20 @@
+import Phaser from 'phaser'
 
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  backgroundColor: '#2d2d2d',
+  scene: {
+    create: create
+  }
+}
+
+function create() {
+  this.add.text(200, 250, 'Кумурска Империясы 🚀', {
+    fontSize: '24px',
+    fill: '#ffffff'
+  })
+}
+
+const game = new Phaser.Game(config)
